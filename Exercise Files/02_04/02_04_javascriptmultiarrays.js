c256029ccd526f43ade019f14f26fbba903b29e9
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2020-04-21 15:46:39 -0400

    Multidim array

--- Exercise Files/02_04/02_04_javascriptmultiarrays.js
+++ Exercise Files/02_04/02_04_javascriptmultiarrays.js
@@ -5,11 +5,11 @@ let mainDishIndex = 1
 
 let firstApp = dinnerChoices[appIndex][0]
 let secondApp = dinnerChoices[appIndex][1]
-let thirdMainDish = dinnerChoices[appIndex][2]
+let thirdMainDish = dinnerChoices[mainDishIndex][2]
 
-console.log(firstApp)
-console.log(secondApp)
-console.log(thirdMainDish)
+console.log(`First appetizer is ${firstApp}`)
+console.log(`Second appetizer is ${secondApp}`)
+console.log(`Third main dish is ${thirdMainDish}`)
 
 dinnerChoices[mainDishIndex][0] = "Steak"
 console.log(dinnerChoices[mainDishIndex][0])

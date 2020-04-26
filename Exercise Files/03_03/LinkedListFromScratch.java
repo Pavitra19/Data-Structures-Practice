1f717537965a597306e6994c77eb712f2ff8683c
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2020-04-26 17:17:22 -0400

    Linked lists in Java

--- Exercise Files/03_03/LinkedListFromScratch.java
+++ Exercise Files/03_03/LinkedListFromScratch.java
@@ -1,19 +1,22 @@
 // Linked List
 public class LinkedListFromScratch {
     Node head;
-    
+
     public void add(int data) {
-        // LL is empty
+        // Linked List is empty
         if (this.head == null) {
+            // set head to the new node created
             this.head = new Node(data);
         } else {
-        // LL is not empty
+            // LL is not empty
+            // Set current head to the next of the new node
+            // Set head to the new node created
             Node nodeToAdd = new Node(data);
             nodeToAdd.next = this.head;
             this.head = nodeToAdd;
         }
     }
-    
+
     public static void main(String[] args) {
         LinkedListFromScratch myList = new LinkedListFromScratch();
         myList.add(10);
@@ -27,6 +30,8 @@ public class LinkedListFromScratch {
 class Node {
     int data;
     Node next;
-    
-    Node(int d) { this.data = d;}
+
+    Node(int d) {
+        this.data = d;
+    }
 }

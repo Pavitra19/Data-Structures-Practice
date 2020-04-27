b6d4ff45780e6681bb8361a682098cf979d65959
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2020-04-26 20:46:44 -0400

    Linked lists

--- Exercise Files/03_04/LinkedListJava.java
+++ Exercise Files/03_04/LinkedListJava.java
@@ -1,27 +1,27 @@
 import java.util.LinkedList;
 
-public class MyClass {
+public class LinkedListJava {
     public static void main(String args[]) {
         LinkedList travelBucketList = new LinkedList();
-        
-        // Add Items
+
+        // Add items
         travelBucketList.add("Santorini, Greece");
         travelBucketList.addFirst("Barcelona, Spain");
         travelBucketList.addLast("Tokyo, Japan");
         travelBucketList.add(2, "Galapagos Islands, Ecuador");
         System.out.println(travelBucketList);
-        
+
         // Access
         System.out.println(travelBucketList.get(2));
         System.out.println(travelBucketList.getFirst());
-        
+
         System.out.println(travelBucketList.contains("Barcelona, Spain"));
-        
-        // Remove Items
+
+        // Remove items
         travelBucketList.removeFirst();
         travelBucketList.removeLast();
         System.out.println(travelBucketList);
-        
+
         travelBucketList.remove("Santorini, Greece");
         travelBucketList.remove(0);
         System.out.println(travelBucketList);

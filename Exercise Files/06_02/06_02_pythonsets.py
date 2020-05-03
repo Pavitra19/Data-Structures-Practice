2d68decc17eda14fb3848e318bd6cf20496476e1
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2020-05-03 15:58:07 -0400

    Sets in Python

--- Exercise Files/06_02/06_02_pythonsets.py
+++ Exercise Files/06_02/06_02_pythonsets.py
@@ -1,12 +1,16 @@
+# finite set
 primaryColors = frozenset(["red", "blue", "yellow"])
 
 color = "green"
 
+# make color lowercase to match case in set
+# check if color is in primary colors set
 if color.lower() in primaryColors:
     print(color + " is a primary color")
 else:
     print(color + " is not a primary color")
 
 letters = set(['a', 'b'])
+# add to set
 letters.add('c')
 print(letters)
